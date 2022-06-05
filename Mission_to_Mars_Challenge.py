143fd1f8f9f42a4dbad8be73c25a7d1007868f2e
CarmiS2 <carmlina.sandoval@gmail.com>
2022-06-05 15:00:16 -0700

    add

--- Mission_to_Mars_Challenge.py
+++ Mission_to_Mars_Challenge.py
@@ -25,11 +25,9 @@ slide_elem = news_soup.select_one('div.list_text')
 slide_elem.find('div', class_='content_title')
 
 
-
 # Use the parent element to find the first `a` tag and save it as `news_title`
 news_title = slide_elem.find('div', class_='content_title').get_text()
 news_title
-
 
 # Use the parent element to find the paragraph text
 news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
@@ -48,14 +46,15 @@ full_image_elem = browser.find_by_tag('button')[1]
 full_image_elem.click()
 
 
-
 # Parse the resulting html with soup
 html = browser.html
 img_soup = soup(html, 'html.parser')
 
+
 # Find the relative image url
 img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
 img_url_rel
+
 
 # Use the base URL to create an absolute URL
 img_url = f'https://spaceimages-mars.com/{img_url_rel}'
@@ -71,4 +70,50 @@ df
 df.to_html()
 
 
+
+# ## D1: Scrape Full-Resolution Mars Hemisphere Images and Titles
+
+
+# 1. Use browser to visit the URL 
+url = 'https://marshemispheres.com/'
+
+browser.visit(url)
+
+
+# 2. Create a list to hold the images and titles.
+hemisphere_image_urls = []
+
+# 3. Write code to retrieve the image urls and titles for each hemisphere.
+for i in range(4):
+    hemispheres = {}
+        
+    browser.find_by_css('a.product-item h3')[i].click()
+    
+    html = browser.html
+    hemi_soup = soup(html,'html.parser')
+    
+    element = browser.links.find_by_text('Sample').first
+    img_url = element['href']
+    
+    title = hemi_soup.find('h2',class_='title').text
+    
+
+    
+    hemispheres["img_url"] = img_url
+    
+    hemispheres["title"] = title
+    
+    hemisphere_image_urls.append(hemispheres)
+    
+    browser.back()
+    
+    
+
+# 4. Print the list that holds the dictionary of each image url and title.
+hemisphere_image_urls
+
+
+# 5. Quit the browser
 browser.quit()
+
+
